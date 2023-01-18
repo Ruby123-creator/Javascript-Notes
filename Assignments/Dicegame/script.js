b2d61d736e6586a6b8ca9879129f7fb9c2226d5a
Ruby123-creator <rubyp20001@gmail.com>
2023-01-18 22:09:27 +0530

    abc

--- Assignments/Dicegame/script.js
+++ Assignments/Dicegame/script.js
@@ -1,15 +1,15 @@
 
-let playerName = document.querySelectorAll("input")
-let btns = document.getElementsByClassName("diceRollButton")
-let scores = document.getElementsByTagName("span")
-
-// [b1,b2,n3,b4,b5,b6]
+let playerName = document.querySelectorAll("input")  //it selects all the input tag element
+let btns = document.getElementsByClassName("diceRollButton")  //it selects all the buttons which have class name diceRollButton
+let scores = document.getElementsByTagName("span") // it selects all the span tag
+// these are arrays which store whole tag element
+// [b1,b2,b3,b4,b5,b6]
 
 // events,  addEventListener
 
+//addEvent
 
-
-for(let i = 0; i<=btns.length-1; i++ ){
+for(let i = 0; i<=btns.length-1; i++ ){    // it iterates over the btn array    
    btns[i].addEventListener("click", ()=>{rollDice(btns[i].id)})
 }
 
---
+++ Assignments/mini_scramble/script.js
@@ -0,0 +1,11 @@
+let word = document.getElementById("evaluatedText");
+let count = document.getElementById("letterCount");
+
+
+word.addEventListener("input" , countlength);
+ function countlength(){
+      
+let evaluated_word = word.value;
+let length = evaluated_word.length;
+count.innerText = length;
+ }
